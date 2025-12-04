388261ab084584ad820ae320c28ae28e72c097d5
Shirinemerwali <shirin.merwali@outlook.com>
2025-12-04 11:55:33 +0100

    components miniräk

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Calculator from './calculator';
 
 function App() {
-  const [a, setA] = useState("");
-  const [b, setB] = useState("");
-  const [result, setResult] = useState("");
-  const [op, setOp] = useState(null);
 
-  function calculate() {
-    const numA = Number(a);
-    const numB = Number(b);
-
-    if (op === "+") {
-      setResult(numA + numB);
-    } else if (op === "-") {
-      setResult(numA - numB);
-    } else if (op === "*") {
-      setResult(numA * numB);
-    } else if (op === "/") {
-      if (numB === 0) {
-        setResult("Error: Division by zero");
-      } else {
-        setResult(numA / numB);
-      }
-    }
-  }
+    
+  
 
   return (
-    <div>
-      <h1>Calculator</h1>
-
-      <input value={a} onChange={(e) => setA(e.target.value)} />
-      <input value={b} onChange={(e) => setB(e.target.value)} />
-
-      <div>
-        <button onClick={() => setOp("+")}>+</button>
-        <button onClick={() => setOp("-")}>-</button>
-        <button onClick={() => setOp("*")}>*</button>
-        <button onClick={() => setOp("/")}>/</button>
-        <button onClick={calculate}>=</button>
-      </div>
-
-      <h2>Result: {result}</h2>
-    </div>
+   <Calculator/>
   );
 }
 
