b812ee2ba3a36806cb77842e3e6bd3892dbac562
Desirée Strand <desireestrxnd@gmail.com>
2025-12-04 11:31:46 +0100

    create user list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,94 @@
 import { useState } from "react";
 import "./App.css";
+import UserList from "./UserList";
 
 function App() {
-  const userArray = [
-    "NullPointerNinja",
-    "AsyncAvenger",
-    "ByteShiftBear",
-    "StackTraceSamurai",
-    "LambdaLurker",
-    "GitPushGuru",
-    "SyntaxSparrow",
-    "DebugDruid",
-    "CryptoCamel",
-    "RefactorRaven",
-  ];
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
 
-  const [users, setUsers] = useState(userArray);
-  const [username, setUsername] = useState("");
+  const addTodo = () => {
+    if (!todos) return;
 
-  const addUser = () => {
-    setUsers([...users, username]);
-    setUsername("");
+    const newTodo = {
+      title,
+      text,
+    };
+    setTodos([...todos, newTodo]);
+    setText("");
+    setTitle("");
   };
 
-  const removeUser = (user) => {
-    let newUserList = users.filter((u) => u !== user);
-    setUsers(newUserList);
-  };
+  const [a, setA] = useState("");
+  const [b, setB] = useState("");
+  const [result, setResult] = useState("");
+  const [op, setOp] = useState(null);
 
-  const sortUsers = () => {
-    let sortedUserList = [...users].sort((a, b) => {
-      const userA = a.toUpperCase();
-      const userB = b.toUpperCase();
-      if (userA < userB) {
-        return -1;
+  function calculate() {
+    const numA = Number(a);
+    const numB = Number(b);
+
+    if (op === "+") {
+      setResult(numA + numB);
+    } else if (op === "-") {
+      setResult(numA - numB);
+    } else if (op === "*") {
+      setResult(numA * numB);
+    } else if (op === "/") {
+      if (numB === 0) {
+        setResult("Error: Division by zero");
+      } else {
+        setResult(numA / numB);
       }
-      if (userA > userB) {
-        return 1;
-      }
-
-      return 0;
-    });
-
-    setUsers(sortedUserList);
-  };
+    }
+  }
 
   return (
     <>
-      <h1>Username app</h1>
-      <input
-        type="text"
-        placeholder="Användarnamn"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <button onClick={addUser}>Lägg till</button>
-      <br /> <br />
-      <h2>Användarlista</h2>
-      <button onClick={() => sortUsers()}>Sortera A-Ö</button>
-      <ul>
-        {users.map((user) => (
-          <li>
-            {user}
-            <button onClick={() => removeUser(user)}>Ta bort</button>
-          </li>
-        ))}
-      </ul>
+      <h1>Projekt</h1>
+      <div className="todo-list">
+        <h2>Todo-list</h2>
+        <input
+          type="text"
+          value={title}
+          placeholder="Titel:"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          value={text}
+          placeholder="Text:"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={() => addTodo()}>Lägg till</button>
+        <ul>
+          {todos?.map((todo) => (
+            <div key={title}>
+              <h3>{todo.title}</h3>
+              <p>{todo.text}</p>
+            </div>
+          ))}
+        </ul>
+      </div>
+
+      <UserList/>
+
+      <div>
+        <h1>Calculator</h1>
+
+        <input value={a} onChange={(e) => setA(e.target.value)} />
+        <input value={b} onChange={(e) => setB(e.target.value)} />
+
+        <div>
+          <button onClick={() => setOp("+")}>+</button>
+          <button onClick={() => setOp("-")}>-</button>
+          <button onClick={() => setOp("*")}>*</button>
+          <button onClick={() => setOp("/")}>/</button>
+          <button onClick={calculate}>=</button>
+        </div>
+
+        <h2>Result: {result}</h2>
+      </div>
     </>
   );
 }
